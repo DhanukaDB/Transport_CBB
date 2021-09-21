1b43cf1602c9492ed787abb13a4542cc93796507
DhanukaDB <69480239+DhanukaDB@users.noreply.github.com>
2021-09-22 01:44:13 +0530

    Insert done

---
+++ app/src/main/java/com/cbb/transportcbb/MainActivity.java
@@ -0,0 +1,32 @@
+package com.cbb.transportcbb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    Button btnsignin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                startActivity(new Intent(MainActivity.this, Signup.class));
+            }
+        });
+
+    }
+}
